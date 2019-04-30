4d888d81893fa41848e3d12f2e0c921dee0ca430
Roman Muehlfeldner <roman@muehlfeldner.net>
2019-04-30 21:32:07 +0200

    Added neo4j to flask microservice

--- flask/app/api.py
+++ flask/app/api.py
@@ -1,16 +1,32 @@
-from flask import Flask
+import os
+from json import dumps
+from flask import Flask, g, Response, request
 from flask_restful import Resource, Api
+from neo4j import GraphDatabase, basic_auth
 
 app = Flask(__name__)
 api = Api(app)
 
-class Product(Resource):
-    def get(self):
-        return {
-            'products': ['Ice cream', 'Chocolate', 'TV', 'Docker Container']
-        }
+password = os.getenv('NEO4J_PASSWORD')
 
-api.add_resource(Product, '/')
+driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth('neo4j', password))
+
+def get_db():
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = driver.session()
+    return g.neo4j_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'neo4j_db'):
+        g.neo4j_db.close()
+
+class Movie(Resource):
+    def get(self):
+        db = get_db()
+        results = db.run("MATCH (")
+
+api.add_resource(Movie, '/movies')
 
 
 if __name__ == '__main__':
